695fa7fe67851e99a97cbfe4fc86714cbcc76b30
Ross Clettenberg <raclettenberg@gmail.com>
2023-07-11 17:43:53 -0500

    Added fetch command for available  meals from firebase. Added a loader for loading that meals data. Styled loader.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MealItem from "./MealItem/MealItem";
-import classes from "./AvailableMeals.module.css"
+import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 
 const DUMMY_MEALS = [
@@ -31,8 +31,51 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => {
-    return <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />
+
+  const [meals, setMeals] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://food-order-app-react-a3ea3-default-rtdb.firebaseio.com/meals.json"
+      );
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.mealsLoading}>
+        <p>Yum! Loading...</p>
+      </section>
+    );
+  }
+  const mealsList = meals.map((meal) => {
+    return (
+      <MealItem
+        id={meal.id}
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    );
   });
 
   return (
